919c1502d0ce3141e24d5f5b848a8350fd111919
micg034 <michelle_go_3@hotmail.com>
2016-07-22 16:22:27 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,3 +25,9 @@ app.controller('PageCtrl', function (/* $scope, $location, $http */) {
     interval: 5000
   });
 });
+
+  // Activates Tooltips for Social Links
+  $('.tooltip-social').tooltip({
+    selector: "a[data-toggle=tooltip]"
+  })
+});
